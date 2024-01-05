a44e24dbbfd1e7e10261681f2ae47d6dd4d6a8a8
Julio Ucharima <julio_ucharima@usmp.pe>
2024-01-05 11:57:08 -0500

    feat: add the barrel file to the components

---
+++ components/index.ts
@@ -0,0 +1,12 @@
+export * from "./about/About";
+export * from "./contact/Contact";
+export * from "./experience/Experience";
+export * from "./footer/Footer";
+export * from "./header/Header";
+export * from "./intro/Introduction";
+
+export * from "./projects";
+export * from "./section";
+
+export * from "./skills/Skills";
+export * from "./switch/ThemeSwitch";
