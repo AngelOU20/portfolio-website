f7c0f57f95058165e2404a4badd73c8461500f18
Julio Ucharima <julio_ucharima@usmp.pe>
2024-01-05 11:45:59 -0500

    feat: export icons for use throughout the application

---
+++ lib/icons.ts
@@ -0,0 +1,49 @@
+import AxiosIcon from "@/public/icons/axios.svg";
+import BootstrapIcon from "@/public/icons/bootstrap.svg";
+import CsharpIcon from "@/public/icons/csharp.svg";
+import CssIcon from "@/public/icons/css.svg";
+import ExpressIcon from "@/public/icons/expressjs.svg";
+import HerokuIcon from "@/public/icons/heroku.svg";
+import HtmlIcon from "@/public/icons/html5.svg";
+import JavaScriptlIcon from "@/public/icons/javascript.svg";
+import JwtIcon from "@/public/icons/jwt.svg";
+import MongoIcon from "@/public/icons/mongodb.svg";
+import NetIcon from "@/public/icons/net.svg";
+import NodeIcon from "@/public/icons/nodejs.svg";
+import PaypalIcon from "@/public/icons/paypal.svg";
+import PostgreSQLIcon from "@/public/icons/postgresql.svg";
+import ReactIcon from "@/public/icons/react.svg";
+import ReactRouterIcon from "@/public/icons/reactrouter.svg";
+import ReduxIcon from "@/public/icons/redux.svg";
+import SassIcon from "@/public/icons/sass.svg";
+import TailwindIcon from "@/public/icons/tailwindcss.svg";
+import TypeScriptIcon from "@/public/icons/typescript.svg";
+import ViteIcon from "@/public/icons/vitejs.svg";
+import MaterialUIIcon from "@/public/icons/materialui.svg";
+import FirebaseIcon from "@/public/icons/firebase.svg";
+
+export const icons = {
+  AxiosIcon,
+  BootstrapIcon,
+  CsharpIcon,
+  CssIcon,
+  ExpressIcon,
+  HerokuIcon,
+  HtmlIcon,
+  JavaScriptlIcon,
+  JwtIcon,
+  MongoIcon,
+  NetIcon,
+  NodeIcon,
+  PaypalIcon,
+  PostgreSQLIcon,
+  ReactIcon,
+  ReactRouterIcon,
+  ReduxIcon,
+  SassIcon,
+  TailwindIcon,
+  TypeScriptIcon,
+  ViteIcon,
+  MaterialUIIcon,
+  FirebaseIcon,
+} as const;
