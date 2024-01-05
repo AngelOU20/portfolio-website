017ef78d4b1a8e6c3c40f60db00f72e458225c91
Julio Ucharima <julio_ucharima@usmp.pe>
2024-01-05 11:53:07 -0500

    feat: add ActiveSection context and provider

---
+++ context/activeSection.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import React, { useState, createContext } from "react";
+import { SectionName } from "@/types/types";
+
+type ActiveSectionProviderProps = {
+  children: React.ReactNode;
+};
+
+type ActiveSectionContextType = {
+  activeSection: SectionName;
+  setActiveSection: React.Dispatch<React.SetStateAction<SectionName>>;
+  timeOfLastClick: number;
+  setTimeOfLastClick: React.Dispatch<React.SetStateAction<number>>;
+};
+
+export const ActiveSectionContext =
+  createContext<ActiveSectionContextType | null>(null);
+
+export function ActiveSectionProvider({
+  children,
+}: ActiveSectionProviderProps) {
+  const [activeSection, setActiveSection] = useState<SectionName>("Home");
+  const [timeOfLastClick, setTimeOfLastClick] = useState(0);
+
+  return (
+    <ActiveSectionContext.Provider
+      value={{
+        activeSection,
+        setActiveSection,
+        timeOfLastClick,
+        setTimeOfLastClick,
+      }}
+    >
+      {children}
+    </ActiveSectionContext.Provider>
+  );
+}
