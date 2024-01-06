db0546cb66375d052167f03a39d674364cb0b2c3
Julio Ucharima <julio_ucharima@usmp.pe>
2024-01-06 11:16:53 -0500

    feat: refactor Home function with loading logic and new components

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,50 @@
-import { Experience } from "../components/experience/Experience";
+"use client";
+
 import {
   Introduction,
   SectionDivider,
   About,
+  Experience,
+  Loader,
   Projects,
   Skills,
   Contact,
+  Header,
+  ThemeSwitch,
+  Footer,
 } from "@/components";
+import { useState } from "react";
 
-export default function Home() {
+export default function Home () {
+  const [isLoading, setIsLoading] = useState(true);
+  const [showContent, setShowContent] = useState(false);
+
+  const handleLoaderLoaded = () => {
+    setIsLoading(false);
+    setTimeout(() => setShowContent(true), 450);
+  };
+
   return (
-    <main className="flex flex-col items-center px-4">
-      <Introduction />
-      <SectionDivider />
-      <About />
-      <Experience />
-      <Skills />
-      <Projects />
-      <Contact />
-    </main>
+    <>
+      {showContent && (
+        <>
+          <Header />
+
+          <main className="flex flex-col items-center px-4 pt-28 sm:pt-36">
+            <Introduction />
+            <SectionDivider />
+            <About />
+            <Experience />
+            <Skills />
+            <Projects />
+            <Contact />
+          </main>
+
+          <ThemeSwitch />
+          <Footer />
+        </>
+      )}
+      <Loader isLoading={isLoading} setIsLoading={handleLoaderLoaded} />
+    </>
   );
 }
