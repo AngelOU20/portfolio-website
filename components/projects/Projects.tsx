4174ffebc570800f0dea80a60eb1f87da821ac05
Julio Ucharima <julio_ucharima@usmp.pe>
2024-01-06 11:27:11 -0500

    refactor: simplify Projects component and add subheading

--- components/projects/Projects.tsx
+++ components/projects/Projects.tsx
@@ -1,25 +1,15 @@
 "use client";
 
-import React, { useRef } from "react";
+import React from "react";
 import { SectionHeading } from "../section/SectionHeading";
 import { mainProjectsData, otherProjectsData } from "@/lib/data";
 import { MainProject } from "./MainProject";
 import { useSectionInView } from "@/hooks";
 import { OtherProject } from "./OtherProject";
-import { motion, useScroll, useTransform } from "framer-motion";
+import { SectionSubHeading } from "../section";
 
-export function Projects() {
+export function Projects () {
   const { ref } = useSectionInView("Projects", 0.55);
-
-  const refMotion = useRef<HTMLDivElement>(null);
-
-  const { scrollYProgress } = useScroll({
-    target: refMotion,
-    offset: ["0 1", "1 1"],
-  });
-
-  const scaleProgess = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
-  const opacityProgess = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
 
   return (
     <section
@@ -27,7 +17,8 @@ export function Projects() {
       className="mb-28 max-w-[44rem] leading-8 sm:mb-40 scroll-mt-28"
       id="projects"
     >
-      <SectionHeading>My Projects</SectionHeading>
+      <SectionHeading>Projects</SectionHeading>
+      <SectionSubHeading>Most recent work</SectionSubHeading>
       <div>
         {mainProjectsData.map((project, index) => (
           <React.Fragment key={index}>
@@ -35,20 +26,13 @@ export function Projects() {
           </React.Fragment>
         ))}
       </div>
-      <motion.div
-        ref={refMotion}
-        style={{
-          scale: scaleProgess,
-          opacity: opacityProgess,
-        }}
-        className="grid grid-cols-1 mt-4 md:grid-cols-2 gap-2 sm:gap-5 sm:mt-8"
-      >
+      <div className="grid grid-cols-1 mt-4 md:grid-cols-2 gap-2 sm:gap-5 sm:mt-8">
         {otherProjectsData.map((project, index) => (
           <React.Fragment key={index}>
             <OtherProject {...project} />
           </React.Fragment>
         ))}
-      </motion.div>
+      </div>
     </section>
   );
 }
