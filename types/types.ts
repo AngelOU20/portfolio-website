685e4d3b53e9632b6edf2d46615640258b1c69f8
Julio Ucharima <julio_ucharima@usmp.pe>
2024-01-16 22:42:51 -0500

    refactor: simplify section name types

--- types/types.ts
+++ types/types.ts
@@ -1,3 +1,5 @@
-import { links } from "@/lib/data";
+import { links as linksEn } from "@/lib/data_en";
+import { links as linksEs } from "@/lib/data";
 
-export type SectionName = (typeof links)[number]["name"];
+export type SectionNameEn = (typeof linksEn)[number]["name"];
+export type SectionNameEs = (typeof linksEs)[number]["name"];
