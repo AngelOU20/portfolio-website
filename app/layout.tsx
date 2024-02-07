4927ab2e2580f391113b171a0223f517457cbaf4
Julio Ucharima <julio_ucharima@usmp.pe>
2024-02-07 16:52:13 -0500

    feat: update color and position to div

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,39 +1,33 @@
-import "./globals.css";
-import { Inter } from "next/font/google";
-import { ActiveSectionProvider } from "@/context/activeSection";
-import { ThemeProvider } from "../context/theme";
-import { Footer, Header, ThemeSwitch } from "@/components";
+import './globals.css';
+import { Inter } from 'next/font/google';
+import { ActiveSectionProvider } from '@/context/activeSection';
+import { ThemeProvider } from '../context/theme';
+import { Footer, Header, ThemeSwitch } from '@/components';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "Julio Ucharima Portfolio",
+  title: 'Julio Ucharima Portfolio',
   description: "I'm a full-stack developer with 1 year of experience.",
   icons: {
-    icon: "/favicon.ico", // /public path
+    icon: '/favicon.ico', // /public path
   },
 };
 
-export default function RootLayout ({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="!scroll-smooth">
       <body
         className={`${inter.className} pt-28 sm:pt-36 relative bg-gray-50 text-gray-900 dark:bg-[#010C15] dark:text-white`}
       >
-        <div className="bg-[#fbe2e3] absolute -z-10 top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[15rem] sm:w-[68.75rem] dark:bg-[#43D9AD] dark:blur-[25rem]"></div>
+        <div className="bg-[#ffdfe0] fixed -z-10 top-[-6rem] right-[0rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[15rem] sm:w-[68.75rem] dark:bg-[#43D9AD] dark:blur-[15rem]"></div>
 
-        <div className="bg-[#dbd7fb] absolute -z-10 top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[15rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#4D5BCE] dark:blur-[20rem]"></div>
+        <div className="bg-[#ddd9ff] fixed -z-10 top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[15rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#5867d6] dark:blur-[15rem]"></div>
 
         <ThemeProvider>
           <ActiveSectionProvider>
             <Header />
-
             {children}
-
             <ThemeSwitch />
             <Footer />
           </ActiveSectionProvider>
