c3fe699edffb9e0e5849e47d228337f0239b0fa8
Julio Ucharima <julio_ucharima@usmp.pe>
2024-02-13 17:05:18 -0500

    chore: update Tailwind CSS configuration to safelist certain classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
   plugins: [],
-  darkMode: "class",
+  darkMode: 'class',
+  safelist: [
+    // Añade las clases que quieres preservar aquí
+    'text-indigo-500',
+    'font-bold',
+    'dark:text-[#12F7D6]',
+    'underline',
+    'italic',
+    // ...
+  ],
 };
