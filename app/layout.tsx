1c2156e2f82bdada1d279e8551bce50c24f3c273
Julio Ucharima <julio_ucharima@usmp.pe>
2024-02-14 21:03:18 -0500

    feat: change background color of the body

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,11 +18,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className="!scroll-smooth">
       <body
-        className={`${inter.className} pt-28 sm:pt-36 relative bg-gray-50 text-gray-900 dark:bg-[#010C15] dark:text-white`}
+        className={`${inter.className} pt-28 sm:pt-36 relative bg-gray-50 text-gray-900 dark:bg-[#0a192f] dark:text-white`}
       >
-        <div className="bg-[#ffdfe0] fixed -z-10 top-[-6rem] right-[0rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[15rem] sm:w-[68.75rem] dark:bg-[#43D9AD] dark:blur-[15rem]"></div>
+        <div className="bg-[#ffdfe0] absolute -z-10 top-[-6rem] right-[0rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[15rem] sm:w-[68.75rem] dark:bg-[#43D9AD] dark:blur-[15rem]"></div>
 
-        <div className="bg-[#ddd9ff] fixed -z-10 top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[15rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#5867d6] dark:blur-[15rem]"></div>
+        <div className="bg-[#ddd9ff] absolute -z-10 top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[15rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#5867d6] dark:blur-[12rem]"></div>
 
         <ThemeProvider>
           <ActiveSectionProvider>
