d8745b0a0ecf1f4d757fc3d816bb46d5fb66c462
Julio Ucharima <julio_ucharima@usmp.pe>
2024-02-19 13:08:57 -0500

    feat: update background color of the dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className="!scroll-smooth">
       <body
-        className={`${inter.className} pt-28 sm:pt-36 relative bg-gray-50 text-gray-900 dark:bg-[#0a192f] dark:text-white`}
+        className={`${inter.className} pt-28 sm:pt-36 relative bg-gray-50 text-gray-900 dark:bg-[#010C15] dark:text-white`}
       >
         <div className="bg-[#ffdfe0] absolute -z-10 top-[-6rem] right-[0rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[15rem] sm:w-[68.75rem] dark:bg-[#43D9AD] dark:blur-[15rem]"></div>
 
