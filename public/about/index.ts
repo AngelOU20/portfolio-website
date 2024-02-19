3d021c4cecb2ff830a936be48fd6483a6ef1968d
Julio Ucharima <julio_ucharima@usmp.pe>
2024-02-19 13:26:40 -0500

    refactor: add barrel file

---
+++ public/about/index.ts
@@ -0,0 +1,15 @@
+import dogWalkingDark from "./dog_walking-dark.svg";
+import dogWalkingLight from "./dog_walking-light.svg";
+import healthyHabitDark from "./healthy_habit-dark.svg";
+import healthyHabitLight from "./healthy_habit-light.svg";
+import programmingDark from "./programming-dark.svg";
+import programmingLight from "./programming-light.svg";
+
+export {
+  dogWalkingDark,
+  dogWalkingLight,
+  healthyHabitDark,
+  healthyHabitLight,
+  programmingDark,
+  programmingLight,
+};
---
+++ public/profile/index.ts
@@ -0,0 +1,4 @@
+import profileDark from "./profile-dark-mode.webp";
+import profileLight from "./profile-light-mode.webp";
+
+export { profileDark, profileLight };
