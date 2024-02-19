43e5447a5d659a9ac2a5a3282a05436bf9959b45
Julio Ucharima <julio_ucharima@usmp.pe>
2024-02-19 16:50:22 -0500

    feat: add font size for subtitle on mobile screen

--- components/section/SectionSubTitle.tsx
+++ components/section/SectionSubTitle.tsx
@@ -6,7 +6,7 @@ type SectionSubTitleProps = {
 
 export function SectionSubTitle({ children }: SectionSubTitleProps) {
   return (
-    <h3 className="text-center text-base mt-1 mb-6 text-indigo-400 dark:text-[#98FAEC]">
+    <h3 className="text-center text-sm sm:text-base mt-1 mb-6 text-indigo-400 dark:text-[#98FAEC]">
       {children}
     </h3>
   );
