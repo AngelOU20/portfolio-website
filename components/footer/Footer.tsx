54998dee8fdf6e0463425ec074f0568391ec6aff
Julio Ucharima <julio_ucharima@usmp.pe>
2024-02-19 18:44:26 -0500

    refactor(Footer): remove 'All rights reserved.' text

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -3,9 +3,7 @@ import React from 'react';
 export function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500 dark:text-gray-400">
-      <small className="mb-2 block text-xs">
-        &copy; 2024 AngelOU20. All rights reserved.
-      </small>
+      <small className="mb-2 block text-xs">&copy; 2024 AngelOU20.</small>
       <p className="text-xs">
         <span className="font-semibold">About this website:</span> built with React &
         Next.js (App Router & Server Actions), TypeScript, Tailwind CSS, Framer Motion,
