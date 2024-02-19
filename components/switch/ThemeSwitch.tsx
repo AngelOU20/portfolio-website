daaa091f9c04ca7a7f1dbe9dbb14ff6095dd56ac
Julio Ucharima <julio_ucharima@usmp.pe>
2024-02-19 14:06:31 -0500

    feat: add animation with motion

--- components/switch/ThemeSwitch.tsx
+++ components/switch/ThemeSwitch.tsx
@@ -1,18 +1,27 @@
-"use client";
+'use client';
 
-import React from "react";
-import { useTheme } from "@/hooks";
-import { BsMoon, BsSun } from "react-icons/bs";
+import React from 'react';
+import { motion } from 'framer-motion';
+import { useTheme } from '@/hooks';
+import { BsMoon, BsSun } from 'react-icons/bs';
 
 export function ThemeSwitch() {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <button
+    <motion.button
       className="fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-900 dark:text-white"
       onClick={toggleTheme}
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      aria-label={
+        theme === 'light'
+          ? 'Button to switch to dark theme'
+          : 'Button to switch to light theme'
+      }
     >
-      {theme === "light" ? <BsSun /> : <BsMoon />}
-    </button>
+      {theme === 'light' ? <BsSun /> : <BsMoon />}
+    </motion.button>
   );
 }
