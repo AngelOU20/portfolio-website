58a5256a42961ea564fa46474d82fb6f26591e96
Julio Ucharima <julio_ucharima@usmp.pe>
2024-06-05 12:55:40 -0500

    feat: add veggie project image import

--- public/projects/index.ts
+++ public/projects/index.ts
@@ -2,5 +2,6 @@ import calendar from "./calendar-app.webp";
 import huertoDelValle from "./huerto-del-valle.webp";
 import journal from "./journal-app.webp";
 import taskboard from "./taskboard.webp";
+import veggie from "./veggie-vault.webp";
 
-export { calendar, huertoDelValle, journal, taskboard };
+export { calendar, huertoDelValle, journal, taskboard, veggie };
